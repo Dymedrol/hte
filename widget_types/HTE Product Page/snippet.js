adbceaf3f5d4336f0dda3bb216dac8efbce11d0c
Dymedrol <dmitrykob@gmail.com>
2025-07-30 18:38:01 +0700

    update

--- widget_types/HTE Product Page/snippet.js
+++ widget_types/HTE Product Page/snippet.js
@@ -1,1 +1,7 @@
+EventBus.subscribe('accessories-rendered:insales:ui_accessories', function (data) {
+    console.log('Была выполнена инициализация компонента ui-accessories', data);
+    $('.accessory-values__item input').click();
+});
 
+console.log('cccccc');
+console.log('!!!', program);
